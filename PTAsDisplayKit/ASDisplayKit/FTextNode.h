d93c7a7459c3ad1130e913f3194b9171a69e8637
净枫 <shaoshuai.ss@alipay.com>
2015-12-28 09:56:37 +0800

    D:[新增TextNode 基类]

---
+++ PTAsDisplayKit/ASDisplayKit/FTextNode.h
@@ -0,0 +1,15 @@
+//
+//  FTextNode.h
+//  PTAsDisplayKit
+//
+//  Created by 净枫 on 15/12/26.
+//  Copyright © 2015年 sunhanpt-pc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "FDisplayNode.h"
+
+
+@interface FTextNode : FDisplayNode
+
+@end
---
+++ PTAsDisplayKit/ASDisplayKit/FTextNode.m
@@ -0,0 +1,13 @@
+//
+//  FTextNode.m
+//  PTAsDisplayKit
+//
+//  Created by 净枫 on 15/12/26.
+//  Copyright © 2015年 sunhanpt-pc. All rights reserved.
+//
+
+#import "FTextNode.h"
+
+@implementation FTextNode
+
+@end
